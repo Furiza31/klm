9e4b78fb9f648edaa23beee4e83307e226a4821a
Furiza31 <hugo.wendjaneh@gmail.com>
2024-09-18 11:05:42 +0200

    fix(package|import path): change package due to vulnerability and change import path in vuejs

--- app/src/pages/auth/Index.vue
+++ app/src/pages/auth/Index.vue
@@ -3,14 +3,13 @@ import Login from "@/components/auth/Login.vue";
 import Register from "@/components/auth/Register.vue";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { useTranslation } from "@/services/Translation";
-import { StringOrNumber } from "node_modules/radix-vue/dist/shared/types";
 import { useRoute, useRouter } from "vue-router";
 
 const { t } = useTranslation();
 const route = useRoute();
 const router = useRouter();
 
-const handleTabChange = (value: StringOrNumber) => {
+const handleTabChange = (value: unknown) => {
   router.push({ name: value as string });
 };
 </script>
