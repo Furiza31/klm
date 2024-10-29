daac10f1e88df9041298d6d690df2f4769a3792c
Furiza31 <hugo.wendjaneh@gmail.com>
2024-10-29 22:34:15 +0100

    feat(deploy): add deploy script

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+# 1. make sure you have the latest version of the code
+git fetch
+git pull
+
+# 2. update database
+npm --prefix ./api run prisma:setup
+
+# 3. stop the server
+docker compose down
+
+# 4. build and restart the server
+docker compose up --build -d
